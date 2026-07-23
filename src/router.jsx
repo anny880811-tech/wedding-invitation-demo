46437a2c470bbeb71a4984447b8341082ac422d5
an <anny880811@gmail.com>
2026-07-23 23:09:00 +0800

    設定 GitHub Pages 部署路徑

--- src/router.jsx
+++ src/router.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 
 export default function Router() {
+  const basename = import.meta.env.BASE_URL === "/" ? undefined : import.meta.env.BASE_URL
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/invite/:guestId" element={<App/>}/>
